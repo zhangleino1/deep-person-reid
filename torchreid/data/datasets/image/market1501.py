f9524df1ea1f8d3c7162aafb346371b575f05f87
zhanglei <851106500@qq.com>
2025-06-16 05:47:56 +0000

    更新Market1501数据集的下载链接为Google Drive

--- torchreid/data/datasets/image/market1501.py
+++ torchreid/data/datasets/image/market1501.py
@@ -21,7 +21,7 @@ class Market1501(ImageDataset):
     """
     _junk_pids = [0, -1]
     dataset_dir = 'market1501'
-    dataset_url = 'http://188.138.127.15:81/Datasets/Market-1501-v15.09.15.zip'
+    dataset_url = 'https://drive.google.com/file/d/0B8-rUzbwVRk0c054eEozWG9COHM/view'
 
     def __init__(self, root='', market1501_500k=False, **kwargs):
         self.root = osp.abspath(osp.expanduser(root))
